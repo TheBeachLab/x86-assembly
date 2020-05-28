f746eae30e403564fdd1c8f0f05d8a57a73a2ce3
Fran <hola@beachlab.org>
2020-05-28 21:32:40 +0200

    hello macro

---
+++ macro/macro.asm
@@ -0,0 +1,24 @@
+; my first x86 assembly language program for x86 processors
+
+section .data
+	text db "hello world",10	; 10 is newline
+
+section .text				; your code (not related to above text)
+	global _start			; link the start of the program
+
+%macro	exit 0
+	;sys_exit(0)
+	mov 	rax, 60			; sys_exit
+	mov 	rdi, 0			; error code
+	syscall
+%endmacro	
+
+_start:					; mandatory start label
+	;sys_write(1,text,12)
+	mov 	rax, 1			; sys_write
+	mov 	rdi, 1			; std output
+	mov 	rsi, text		; memory address of the buffer
+	mov 	rdx, 12			; length of the string, including newline
+	syscall				; call the kernel				
+
+	exit
