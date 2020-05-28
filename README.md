ec98086ffd844acd333d820cfb16d8462c19ab5b
Fran <hola@beachlab.org>
2020-05-28 14:16:09 +0200

    hello world explained

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # x86-assembly
+
 My adventures learning x86-assembly
+
+## Hello world
+
+- Create an object `nasm -f elf64 -o hello.o hello.asm`
+- Link it `ld hello.o -o hello`
+
+
--- hello.asm
+++ hello.asm
@@ -1,19 +1,20 @@
 ; my first x86 assembly language program for x86 processors
 
 section .data
-	text db "hello world",10
+	text db "hello world",10	; 10 is newline
 
-section .text
-	global _start
+section .text				; your code (not related to above text)
+	global _start			; link the start of the program
 
-_start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, text
-	mov rdx, 14
-	syscall
-
-	mov rax, 60
-	mov rdi, 0
+_start:					; mandatory start label
+	;sys_write(1,text,12)
+	mov 	rax, 1			; sys_write
+	mov 	rdi, 1			; std output
+	mov 	rsi, text		; memory address of the buffer
+	mov 	rdx, 12			; length of the string, including newline
+	syscall				; call the kernel				
+	;sys_exit(0)
+	mov 	rax, 60			; sys_exit
+	mov 	rdi, 0			; error code
 	syscall
 
